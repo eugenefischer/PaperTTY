b6329c823bee14f118c6bcc5d02bbb15bf6d3a8a
eugenefischer <66030419+eugenefischer@users.noreply.github.com>
2020-10-27 12:53:43 -0500

    Adding code from rpi_epd2in7

--- papertty/drivers/drivers_partial.py
+++ papertty/drivers/drivers_partial.py
@@ -202,6 +202,65 @@ class WavesharePartial(WaveshareEPD):
             
 
 class rpi_epd2in7(object)
+    RST_PIN         = 17
+    DC_PIN          = 25
+    CS_PIN          = 8
+    BUSY_PIN        = 24
+    
+    
+    # Display resolution
+    EPD_WIDTH       = 176
+    EPD_HEIGHT      = 264   
+
+    # EPD2IN7 commands
+    # Specifciation: https://www.waveshare.com/w/upload/2/2d/2.7inch-e-paper-Specification.pdf
+    PANEL_SETTING                               = 0x00
+    POWER_SETTING                               = 0x01
+    POWER_OFF                                   = 0x02
+    POWER_OFF_SEQUENCE_SETTING                  = 0x03
+    POWER_ON                                    = 0x04
+    POWER_ON_MEASURE                            = 0x05
+    BOOSTER_SOFT_START                          = 0x06
+    DEEP_SLEEP                                  = 0x07
+    DATA_START_TRANSMISSION_1                   = 0x10
+    DATA_STOP                                   = 0x11
+    DISPLAY_REFRESH                             = 0x12
+    DATA_START_TRANSMISSION_2                   = 0x13
+    PARTIAL_DATA_START_TRANSMISSION_1           = 0x14
+    PARTIAL_DATA_START_TRANSMISSION_2           = 0x15
+    PARTIAL_DISPLAY_REFRESH                     = 0x16
+    LUT_FOR_VCOM                                = 0x20
+    LUT_WHITE_TO_WHITE                          = 0x21
+    LUT_BLACK_TO_WHITE                          = 0x22
+    LUT_WHITE_TO_BLACK                          = 0x23
+    LUT_BLACK_TO_BLACK                          = 0x24
+    PLL_CONTROL                                 = 0x30
+    TEMPERATURE_SENSOR_COMMAND                  = 0x40
+    TEMPERATURE_SENSOR_CALIBRATION              = 0x41
+    TEMPERATURE_SENSOR_WRITE                    = 0x42
+    TEMPERATURE_SENSOR_READ                     = 0x43
+    VCOM_AND_DATA_INTERVAL_SETTING              = 0x50
+    LOW_POWER_DETECTION                         = 0x51
+    TCON_SETTING                                = 0x60
+    TCON_RESOLUTION                             = 0x61
+    SOURCE_AND_GATE_START_SETTING               = 0x62
+    GET_STATUS                                  = 0x71
+    AUTO_MEASURE_VCOM                           = 0x80
+    VCOM_VALUE                                  = 0x81
+    VCM_DC_SETTING_REGISTER                     = 0x82
+    PROGRAM_MODE                                = 0xA0
+    ACTIVE_PROGRAM                              = 0xA1
+    READ_OTP_DATA                               = 0xA2
+
+
+    def _nearest_mult_of_8(number, up=True):
+    """ Find the nearest multiple of 8, rounding up or down """
+        if up:
+            return ((number + 7) // 8) * 8
+        else:
+            return (number // 8) * 8
+
+    
     #code from the rpi_epd2in7 partial update driver by Elad Alfassa
     def __init__(self, partial_refresh_limit=32, fast_refresh=True):
         """ Initialize the EPD class.
